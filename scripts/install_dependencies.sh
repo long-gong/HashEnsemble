ab5d5bc55ced3488732f77ee8e6292dc4034ed5e
Long Gong <long.github@gmail.com>
2020-06-25 10:33:32 -0400

    fixed install scripts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,17 +4,6 @@ set -e
 
 sudo apt-get install -y libboost-serialization-dev libboost-container-dev valgrind
 
-cd /tmp
-rm -rf dynamic_bitset
-git clone https://github.com/boostorg/dynamic_bitset.git
-cd dynamic_bitset
-mkdir build && cd build
-cmake ..
-make -j4 VERBOSE=1
-sudo make install
-cd /tmp
-rm -rf dynamic_bitset
-
 rm -rf googletest
 git clone https://github.com/google/googletest.git
 cd googletest
