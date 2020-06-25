ef3f2ffc42f522da5443f90633e51a60cf116a9a
Long Gong <long.github@gmail.com>
2020-06-25 10:37:18 -0400

    fixed install scripts remove verbose

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -9,7 +9,7 @@ git clone https://github.com/google/googletest.git
 cd googletest
 mkdir build && cd build
 cmake ..
-make -j4 VERBOSE=1
+make -j4
 sudo make install
 cd /tmp
 rm -rf googletest
@@ -19,7 +19,7 @@ git clone https://github.com/fmtlib/fmt.git
 cd fmt
 mkdir build && cd build
 cmake ..
-make -j4 VERBOSE=1
+make -j4
 sudo make install
 cd /tmp
 rm -rf fmt
